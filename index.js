d52479e7af64a49ce3c872897443300a26897be5
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-19 15:07:53 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+function findMatching(driver , matchName){
+  let matcher = driver.filter(drivers => drivers.toLowerCase() === matchName.toLowerCase())
+  return matcher;
+
+}
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+console.log(findMatching(drivers , 'Bobby'))
+
+function fuzzyMatch(array , string){
+  let find = array.filter(driver => driver.startsWith(string))
+  return find
+
+}
+let ahmed = fuzzyMatch(drivers , 'B')
+console.log(ahmed)
+
+function matchName(array, string) {
+  return array.filter(driver => driver.name === string);
+}
+let me = matchName(drivers , 'Los Angeles')
